e46208916bf3419ca8768b77e58a66dcc92a972c
Andrew Hale <andrew.z.hale1@gmail.com>
2019-11-04 14:04:06 -0800

    Initial Commit

---
+++ UI-Portfolio/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UI-Portfolio
+//
+//  Created by Hale, Andrew on 11/4/19.
+//  Copyright © 2019 Hale, Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ UI-Portfolio/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  UI-Portfolio
+//
+//  Created by Hale, Andrew on 11/4/19.
+//  Copyright © 2019 Hale, Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
